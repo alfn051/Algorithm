82a42b3fc83ef81fe96d19414e2d0d09adaad154
Miru <57497671+alfn051@users.noreply.github.com>
2024-01-31 16:57:10 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.61 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        LinkedList<Integer> bridge = new LinkedList<>();
+        LinkedList<Integer> trucks = new LinkedList<>();
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
+        }
+        for (int i : truck_weights) {
+            trucks.offer(i);
+        }
+        int answer = 0;
+
+        while (!trucks.isEmpty()) {
+            bridge.poll();
+            answer++;
+            if (bridge.stream().mapToInt(i -> i).sum() + trucks.peek() <= weight) {
+                bridge.offer(trucks.poll());
+            }else {
+                bridge.offer(0);
+            }
+        }
+        while (!bridge.isEmpty()) {
+            bridge.poll();
+            answer++;
+        }
+
+        return answer;
+    }
+}
