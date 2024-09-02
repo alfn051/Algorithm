88d9b67e28c9f4e3907ba3abe98d9250650f94cc
Miru <57497671+alfn051@users.noreply.github.com>
2024-09-02 17:13:13 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int r = Integer.parseInt(br.readLine());
+        for(int i=r;i>=1;i--){
+            StringBuilder sb = new StringBuilder();
+            sb.append(" ".repeat(r-i));
+            sb.append("*".repeat(i*2-1));
+            bw.write(sb+"\n");
+        }
+        for(int i=2;i<=r;i++){
+            StringBuilder sb = new StringBuilder();
+            sb.append(" ".repeat(r-i));
+            sb.append("*".repeat(i*2-1));
+            bw.write(sb+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
