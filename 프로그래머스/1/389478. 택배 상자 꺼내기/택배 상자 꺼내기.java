814a867ffb9f1a5a6817f95a2891a7ea9957c326
Miru <57497671+alfn051@users.noreply.github.com>
2025-05-05 21:37:17 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        
-        
-        int numl = (num - 1) / w % 2 == 0 ? (num - 1) % w : w - 1 - (num - 1) % w;
-        int nl = (n - 1) / w % 2 == 0 ? (n - 1) % w : w - 1 - (n - 1) % w;
-
-        return (n - 1) / w % 2 == 0 ? (numl > nl ? (n - 1) / w - (num - 1) / w : (n - 1) / w - (num - 1) / w + 1) : (numl < nl ? (n - 1) / w - (num - 1) / w : (n - 1) / w - (num - 1) / w + 1);
+        int numl = (num-1)/w%2==0?(num-1)%w:w-1-(num-1)%w;
+        int nl = (n-1)/w%2==0?(n-1)%w:w-1-(n-1)%w;
+        return (n-1)/w%2==0?(numl>nl?(n-1)/w-(num-1)/w:(n-1)/w-(num-1)/w+1):(numl<nl?(n-1)/w-(num-1)/w:(n-1)/w-(num-1)/w+1);
     }
 }
